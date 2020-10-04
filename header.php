2159011433c6b8cbec0405053977090011387247
ramanmehta7 <ramanmehta833@gmail.com>
2020-10-05 01:44:48 +0530

    Start

---
+++ header.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+
+<!--    MDB-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+
+<!--    Customized-->
+    <link rel="stylesheet" href="CSS/common.css">
+</head>
+</html>
+
+<!--git push https://ramanmehta7:PASSWORD@github.com/ramanmehta7/shivmayhospital.git-->
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include("header.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Shivmay Hospitals</title>
+</head>
+
+<body>
+
+</body>
+
+<?php include("footer.php"); ?>
+<script src="JavaScript/home.js"></script>
+</html>
